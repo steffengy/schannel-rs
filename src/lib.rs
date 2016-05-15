9a45f4b58f9cbfd3b5b3103b5da1795e278c5a17
Steven Fackler <sfackler@gmail.com>
2016-05-15 14:14:47 -0700

    Handshake

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ extern crate winapi;
 
 use libc::c_ulong;
 use secur32::{AcquireCredentialsHandleA, FreeCredentialsHandle, InitializeSecurityContextW,
-              DeleteSecurityContext, FreeContextBuffer};
+              DeleteSecurityContext, FreeContextBuffer, QueryContextAttributesW};
+use std::cmp;
 use std::error;
 use std::fmt;
 use std::io::{self, Read, Write, Cursor};
@@ -18,9 +19,8 @@ use winapi::{CredHandle, SECURITY_STATUS, SCHANNEL_CRED, SCHANNEL_CRED_VERSION, 
              ISC_REQ_CONFIDENTIALITY, ISC_REQ_INTEGRITY, ISC_REQ_REPLAY_DETECT,
              ISC_REQ_SEQUENCE_DETECT, ISC_REQ_ALLOCATE_MEMORY, ISC_REQ_STREAM, SecBuffer,
              SECBUFFER_EMPTY, SECBUFFER_TOKEN, SecBufferDesc, SECBUFFER_VERSION,
-             SEC_I_CONTINUE_NEEDED};
-
-const MAX_RECORD_SIZE: usize = 16 * 1024;
+             SEC_I_CONTINUE_NEEDED, SecPkgContext_StreamSizes, SECPKG_ATTR_STREAM_SIZES,
+             SECBUFFER_ALERT, SECBUFFER_EXTRA, SEC_E_INCOMPLETE_MESSAGE};
 
 const INIT_REQUESTS: c_ulong = ISC_REQ_CONFIDENTIALITY |
                 ISC_REQ_INTEGRITY | 
@@ -120,7 +120,7 @@ impl TlsStreamBuilder {
         self
     }
 
-    pub fn initialize<S>(&self, cred: SchannelCred, mut stream: S) -> io::Result<TlsStream<S>>
+    pub fn initialize<S>(&self, cred: SchannelCred, stream: S) -> io::Result<TlsStream<S>>
         where S: Read + Write
     {
         let (ctxt, buf) = try!(SecurityContext::initialize(&cred,
@@ -133,16 +133,20 @@ impl TlsStreamBuilder {
             domain: self.domain.clone(),
             stream: stream,
             state: State::Initializing,
-            plaintext_in_buf: Cursor::new(Vec::with_capacity(MAX_RECORD_SIZE)),
-            encrypted_in_buf: Vec::with_capacity(MAX_RECORD_SIZE),
-            plaintext_out_buf: Cursor::new(Vec::with_capacity(MAX_RECORD_SIZE)),
-            encrypted_out_buf: Cursor::new(Vec::with_capacity(MAX_RECORD_SIZE)),
+            plaintext_in_buf: Cursor::new(Vec::new()),
+            encrypted_in_buf: Cursor::new(Vec::new()),
+            out_buf: Cursor::new(buf.to_owned()),
         };
-
-        stream.encrypted_out_buf.get_mut().extend_from_slice(&buf);
+        try!(stream.initialize());
 
         Ok(stream)
     }
+}
+
+enum InitializeResponse {
+    ContinueNeeded(usize, ContextBuffer),
+    IncompleteMessage,
+    Ok(usize, Option<ContextBuffer>),
 }
 
 struct SecurityContext(CtxtHandle);
@@ -188,6 +192,108 @@ impl SecurityContext {
             Ok((SecurityContext(ctxt), ContextBuffer(outbuf)))
         }
     }
+
+    fn continue_initialize(&mut self,
+                            cred: &SchannelCred,
+                            domain: Option<&[u16]>,
+                            buf: &mut [u8])
+                            -> Result<InitializeResponse> {
+        unsafe {
+            let domain = domain.map(|b| b.as_ptr() as *mut u16).unwrap_or(ptr::null_mut());
+
+            let inbufs = &mut [SecBuffer {
+                                   cbBuffer: buf.len() as c_ulong,
+                                   BufferType: SECBUFFER_TOKEN,
+                                   pvBuffer: buf.as_mut_ptr() as *mut _,
+                               },
+                               SecBuffer {
+                                   cbBuffer: 0,
+                                   BufferType: SECBUFFER_EMPTY,
+                                   pvBuffer: ptr::null_mut(),
+                               }];
+            let mut inbuf_desc = SecBufferDesc {
+                ulVersion: SECBUFFER_VERSION,
+                cBuffers: 2,
+                pBuffers: inbufs.as_mut_ptr(),
+            };
+
+            let outbufs = &mut [SecBuffer {
+                                    cbBuffer: 0,
+                                    BufferType: SECBUFFER_TOKEN,
+                                    pvBuffer: ptr::null_mut(),
+                                },
+                                SecBuffer {
+                                    cbBuffer: 0,
+                                    BufferType: SECBUFFER_ALERT,
+                                    pvBuffer: ptr::null_mut(),
+                                }];
+            let mut outbuf_desc = SecBufferDesc {
+                ulVersion: SECBUFFER_VERSION,
+                cBuffers: 2,
+                pBuffers: outbufs.as_mut_ptr(),
+            };
+
+            let mut attributes = 0;
+
+            let status = InitializeSecurityContextW(&cred.0 as *const _ as *mut _,
+                                                    &mut self.0,
+                                                    domain,
+                                                    INIT_REQUESTS,
+                                                    0,
+                                                    0,
+                                                    &mut inbuf_desc,
+                                                    0,
+                                                    ptr::null_mut(),
+                                                    &mut outbuf_desc,
+                                                    &mut attributes,
+                                                    ptr::null_mut());
+
+            if !outbufs[1].pvBuffer.is_null() {
+                FreeContextBuffer(outbufs[1].pvBuffer);
+            }
+
+            match status {
+                SEC_I_CONTINUE_NEEDED => {
+                    let nread = if inbufs[1].BufferType == SECBUFFER_EXTRA  {
+                        buf.len() - inbufs[1].cbBuffer as usize
+                    } else {
+                        buf.len()
+                    };
+                    let to_write = ContextBuffer(outbufs[0]);
+                    Ok(InitializeResponse::ContinueNeeded(nread, to_write))
+                }
+                SEC_E_INCOMPLETE_MESSAGE => Ok(InitializeResponse::IncompleteMessage),
+                SEC_E_OK => {
+                    let nread = if inbufs[1].BufferType == SECBUFFER_EXTRA  {
+                        buf.len() - inbufs[1].cbBuffer as usize
+                    } else {
+                        buf.len()
+                    };
+                    let to_write = if outbufs[0].pvBuffer.is_null() {
+                        None
+                    } else {
+                        Some(ContextBuffer(outbufs[0]))
+                    };
+                    Ok(InitializeResponse::Ok(nread, to_write))
+                }
+                _ => Err(Error(status))
+            }
+        }
+    }
+
+    fn stream_sizes(&self) -> Result<SecPkgContext_StreamSizes> {
+        unsafe {
+            let mut stream_sizes = mem::uninitialized();
+            let status = QueryContextAttributesW(&self.0 as *const _ as *mut _,
+                                                 SECPKG_ATTR_STREAM_SIZES,
+                                                 &mut stream_sizes as *mut _ as *mut _);
+            if status == SEC_E_OK {
+                Ok(stream_sizes)
+            } else {
+                Err(Error(status))
+            }
+        }
+    }
 }
 
 impl Drop for SecurityContext {
@@ -224,9 +330,8 @@ pub struct TlsStream<S> {
     stream: S,
     state: State,
     plaintext_in_buf: Cursor<Vec<u8>>,
-    encrypted_in_buf: Vec<u8>,
-    plaintext_out_buf: Cursor<Vec<u8>>,
-    encrypted_out_buf: Cursor<Vec<u8>>,
+    encrypted_in_buf: Cursor<Vec<u8>>,
+    out_buf: Cursor<Vec<u8>>,
 }
 
 impl<S> TlsStream<S>
@@ -238,6 +343,117 @@ impl<S> TlsStream<S>
 
     pub fn get_mut(&mut self) -> &mut S {
         &mut self.stream
+    }
+
+    fn initialize(&mut self) -> io::Result<()> {
+        let mut needs_read = true;
+
+        while let State::Initializing = self.state {
+            if try!(self.write_out()) > 0 {
+                try!(self.stream.flush());
+            }
+
+            if needs_read {
+                if try!(self.read_in()) == 0 {
+                    return Err(io::Error::new(io::ErrorKind::UnexpectedEof,
+                                              "unexpected EOF during handshake"));
+                }
+            }
+
+            let end = self.encrypted_in_buf.position() as usize;
+            match self.context.continue_initialize(&self.cred,
+                                                   self.domain.as_ref().map(|d| &d[..]),
+                                                   &mut self.encrypted_in_buf.get_mut()[..end]) {
+                Ok(InitializeResponse::ContinueNeeded(nread, buf)) => {
+                    self.consume_encrypted_in(nread);
+                    self.out_buf.get_mut().extend_from_slice(&buf);
+                    needs_read = self.encrypted_in_buf.get_ref().len() == 0;
+                }
+                Ok(InitializeResponse::Ok(nread, buf)) => {
+                    self.consume_encrypted_in(nread);
+                    if let Some(buf) = buf {
+                        self.out_buf.get_mut().extend_from_slice(&buf);
+                    }
+                    // FIXME try to decrypt if encrypted_in_buf is nonempty
+                    self.state = State::Streaming;
+                }
+                Ok(InitializeResponse::IncompleteMessage) => needs_read = true,
+                Err(e) => return Err(io::Error::new(io::ErrorKind::Other, e)),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn write_out(&mut self) -> io::Result<usize> {
+        let mut out = 0;
+        while self.out_buf.position() as usize != self.out_buf.get_ref().len() {
+            let position = self.out_buf.position() as usize;
+            let nwritten = try!(self.stream.write(&self.out_buf.get_ref()[position..]));
+            out += nwritten;
+            self.out_buf.set_position((position + nwritten) as u64);
+        }
+
+        self.out_buf.set_position(0);
+        self.out_buf.get_mut().clear();
+
+        Ok(out)
+    }
+
+    fn read_in(&mut self) -> io::Result<usize> {
+        let existing_len = self.encrypted_in_buf.position() as usize;
+        let min_len = cmp::max(1024, 2 * existing_len);
+        if self.encrypted_in_buf.get_ref().len() < min_len {
+            self.encrypted_in_buf.get_mut().resize(min_len, 0);
+        }
+        let nread = {
+            let buf = &mut self.encrypted_in_buf.get_mut()[existing_len..];
+            try!(self.stream.read(buf))
+        };
+        self.encrypted_in_buf.set_position((existing_len + nread) as u64);
+        Ok(nread)
+    }
+
+    fn consume_encrypted_in(&mut self, nread: usize) {
+        unsafe {
+            let src = &self.encrypted_in_buf.get_ref()[nread] as *const _;
+            let dst = self.encrypted_in_buf.get_mut().as_mut_ptr();
+
+            let size = self.encrypted_in_buf.position() as usize;
+            assert!(size >= nread);
+            let count = size - nread;
+
+            ptr::copy(src, dst, count);
+
+            self.encrypted_in_buf.set_position(count as u64);
+        }
+    }
+}
+
+impl<S> Write for TlsStream<S>
+    where S: Read + Write
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        try!(self.initialize());
+        unimplemented!();
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+impl<S> Read for TlsStream<S>
+    where S: Read + Write
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if self.plaintext_in_buf.position() as usize != self.plaintext_in_buf.get_ref().len() {
+            return self.plaintext_in_buf.read(buf);
+        }
+
+        try!(self.initialize());
+
+        unimplemented!();
     }
 }
 
